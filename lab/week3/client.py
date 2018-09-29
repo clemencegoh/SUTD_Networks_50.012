46923da2ffbe03ce9f1e892d8baeede60dd7ec41
Clemence Goh <clemencegoh@users.noreply.github.com>
2018-09-29 16:18:00 +0800

    having fun with missing data

--- lab/week3/client.py
+++ lab/week3/client.py
@@ -8,6 +8,7 @@ import socket as S
 import argparse
 import json
 import random
+import time
 
 def generateRandomString(_length):
     alphaLower = "abcdefghijklmnopqrstuvwxyz"
@@ -20,22 +21,23 @@ def generateRandomString(_length):
         toReturn += random.choice(allPossible)
     return toReturn
 
-def commandLineArgs():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-r', type=float, dest='rate',
-        help='Packet rate in Mbps (eg; -r 1.5 is 1.5 Mbps)')
 
-    args = parser.parse_args()
-    rate = 3.0
+def commandLineArgs(_rate):
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument('-r', type=float, dest='rate',
+    #     help='Packet rate in Mbps (eg; -r 1.5 is 1.5 Mbps)')
 
-    if args.rate == None:
+    # args = parser.parse_args()
+    default_rate = 3.0
+
+    if _rate == None:
         # print("USAGE:")
         # print("python2 client.py -r 3.0:")
-        print("using default {} Mbps...".format(rate))
-        return rate
+        print("using default {} Mbps...".format(default_rate))
+        return default_rate
     else:
-        print("Client rate is {} Mbps.".format(args.rate))
-        return args.rate
+        print("Client rate is {} Mbps.".format(_rate))
+        return _rate
 
 
 def createPayload(_message, _ID):
@@ -45,20 +47,45 @@ def createPayload(_message, _ID):
     }
 
 
-def clientSide():
-    rate = commandLineArgs()
+def clientSide(_rate=None):
+    rate = commandLineArgs(_rate)
     host_name = "localhost"
     port_number = 5555
 
     sock = S.socket(S.AF_INET, S.SOCK_DGRAM)
     server_address = (host_name, port_number)
 
-    for i in range(100):
-        if i != 12:
+    # number to be missed
+    missing_numbers = [12, 18, 21, 30]
+    # number of data to be sent
+    max_number = int(rate * 1000)
+    total_data = 0
+
+    # timer
+    start_time = time.time()
+
+    # client-side counting
+    for i in range(max_number):
+        if i not in missing_numbers:
             message = createPayload(generateRandomString(12), i)
 
-            print("Sent with Payload: {} \nID: {}".format(message, message["ID"]))
-            sent = sock.sendto(json.dumps(message).encode(), server_address)
+            # print("Sent with Payload: {} \nID: {}".format(message, message["ID"]))
+            total_data += sock.sendto(json.dumps(message).encode(), server_address)
+
+    end_time = time.time()
+
+    sock.sendto(json.dumps({"ID": max_number, "Payload": "End"}).encode(), server_address)
+
+    # Report
+    total_time_taken = end_time - start_time
+    print("--------- Report ----------")
+    print("Total bytes sent:", str(total_data))
+    print("Total time taken:", str(total_time_taken))
+    print("Final count:", max_number)
+    print("------- End Report --------")
+
+
+
 
 
 if __name__=="__main__":
--- lab/week3/server.py
+++ lab/week3/server.py
@@ -12,21 +12,36 @@ def serverSide():
     sock = S.socket(S.AF_INET, S.SOCK_DGRAM)
     sock.bind(('localhost', 5555))
 
+    # sever side counting of objects
     serverCounter = -1
+    missed_data = []
 
     while True:
         serverCounter += 1
         data, addrs = sock.recvfrom(4096)
         data = json.loads(data.decode())
 
+        if data["Payload"] == "End":
+            break
+
         if serverCounter != data["ID"]:
-            print("WARNING: ID {} not received\n\n".format(serverCounter))
+            # print("WARNING: ID {} not received\n\n".format(serverCounter))
+            missed_data.append("ID: {}".format(serverCounter))
+
             # skip count
             serverCounter += 1
 
         # debugger
-        print("Payload: {} \nID: {} \nAddresses: {} \n\n".
-              format(data["Payload"], data["ID"], addrs))
+        # print("Payload: {} \nID: {} \nAddresses: {} \n\n".
+        #       format(data["Payload"], data["ID"], addrs))
+
+    print("--------- Report ---------")
+    print("Total expected:", data["ID"])
+    if missed_data:
+        print("! WARNING !")
+        print("Data missed:", str(missed_data))
+        print("! WARNING !")
+    print("------- End Report -------")
 
 
 
